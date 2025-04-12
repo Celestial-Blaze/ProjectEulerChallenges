6d0d1e9148cdc43666289a0b77f8472419fcf7dc
Celestial Blaze <152753428+Celestial-Blaze@users.noreply.github.com>
2025-04-12 00:51:45 -0400

    Create counting_sundays.py

---
+++ counting_sundays.py
@@ -0,0 +1,15 @@
+# how many sundays fell on the first of the month between two given dates
+# other info: 1st Jan 1900 was a Monday
+# a leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400
+
+# Input format:
+# Each testcase (t) will contain two lines
+# Y1 space M1 space D1 on first line denoting starting date
+# Y2 space M2 space D2 on second line denoting ending date
+
+# Constraints:
+# 1 <= t <= 100
+# 1900 <= Y1 <= 1e16
+# Y1 <= Y2 <= (Y1 + 1000)
+# 1 <= M1, M2 <= 12
+# 1 <= D1, D2 <= 31
